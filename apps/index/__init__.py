be452fb70b71cb915b810b05e28ccba2e80b5dd2
软软软 <soft@ruanruanruandeMacBook-Pro.local>
2019-11-03 18:34:47 +0800

    新增bootstrap

--- apps/index/__init__.py
+++ apps/index/__init__.py
@@ -2,5 +2,8 @@ from flask.blueprints import Blueprint
 from utils.import_lib import register
 
 
-ix = Blueprint("index", __name__, template_folder="./templates", static_folder="./static")
+ix = Blueprint("index", __name__,
+               template_folder="./templates",
+               static_folder="./static",
+               static_url_path="/index/static/")
 register("apps.index.views")
